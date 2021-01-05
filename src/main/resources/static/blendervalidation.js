8f9df0b25d9c081569dfc595af267b3f7c9a9a5c
Luuk Wempe <luukwempe@hotmail.com>
2021-01-05 16:35:52 +0100

    started validation of the input fields.

--- src/main/resources/static/blendervalidation.js
+++ src/main/resources/static/blendervalidation.js
@@ -7,8 +7,8 @@ $(document)
                         identifier: 'c1_o2',
                         rules: [
                             {
-                                type: 'integer[0..100]',
-                                prompt: 'Please enter a number between 0 - 100'
+                                type: 'regExp[[0-9]{1,3}(\.[0-9]{0,1})?]',
+                                prompt: 'Current Oxygen value invalid, please enter a number between 0 - 100'
                             }
                         ]
                     },
@@ -16,11 +16,72 @@ $(document)
                         identifier: 'c2_o2',
                         rules: [
                             {
-                                type: 'integer[0..100]',
+                            	type: 'regExp[[0-9]{1,3}(\.[0-9]{0,1})?]',
+                                prompt: 'Please enter a number between 0 - 100'
+                            }
+                        ]
+                    },
+                    
+                    c1_n2: {
+                        identifier: 'c1_n2',
+                        rules: [
+                            {
+                            	type: 'regExp[[0-9]{1,3}(\.[0-9]{0,1})?]',
+                                prompt: 'Please enter a number between 0 - 100'
+                            }
+                        ]
+                    },
+                    
+                    c2_n2: {
+                        identifier: 'c2_n2',
+                        rules: [
+                            {
+                            	type: 'regExp[[0-9]{1,3}(\.[0-9]{0,1})?]',
+                                prompt: 'Please enter a number between 0 - 100'
+                            }
+                        ]
+                    },
+                    
+                    c1_he: {
+                        identifier: 'c1_he',
+                        rules: [
+                            {
+                            	type: 'regExp[[0-9]{1,3}(\.[0-9]{0,1})?]',
+                                prompt: 'Please enter a number between 0 - 100'
+                            }
+                        ]
+                    },
+
+                    c2_he: {
+                        identifier: 'c2_he',
+                        rules: [
+                            {
+                            	type: 'regExp[[0-9]{1,3}(\.[0-9]{0,1})?]',
+                                prompt: 'Please enter a number between 0 - 100'
+                            }
+                        ]
+                    },
+                    
+                    c1_size: {
+                        identifier: 'c2_size',
+                        rules: [
+                            {
+                            	type: 'regExp[(([0-3][0-9]{1,2})|(40)|[0-9])(\.[0-9]{0,1})?]',
+                                prompt: 'Please enter a number between 0 - 100'
+                            }
+                        ]
+                    },
+                    c2_size: {
+                        identifier: 'c2_size',
+                        rules: [
+                            {
+                                type: 'regExp[(([0-3][0-9]{1,2})|(40)|[0-9])(\.[0-9]{0,1})?]',
                                 prompt: 'Please enter a number between 0 - 100'
                             }
                         ]
                     }
+
+
 
                 }
             })
@@ -28,3 +89,4 @@ $(document)
 
     })
 ;
+
